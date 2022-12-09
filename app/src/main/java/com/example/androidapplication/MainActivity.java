f7c0b23d0747fc21790defba5471eacdc8bb2be5
Bheemarasetty Deepak <deepakprime4719+github@gmail.com>
2022-12-09 21:34:05 +0530

    Added a Play and Pause Feature for the Timer

--- app/src/main/java/com/example/androidapplication/MainActivity.java
+++ app/src/main/java/com/example/androidapplication/MainActivity.java
@@ -31,35 +31,54 @@ public class MainActivity extends AppCompatActivity {
     FragmentAdapter fragmentAdapter;
     Button playButton;
     Iterator itr;
+    View timerLayoutView;
+    PopupWindow timerLayout;
+    CountDownTimer startTimer;
+    CountDownTimer restTimer;
+    int activeCounter;
+    int restCounter;
+    boolean timerRunning = false;
+    TextView countdown;
+    TextView currentActivity;
+    TextView nextActivity;
+    Button pauseButton;
+    Button resetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fragmentArrayList.add(new Fragment("Fragment1", 10, 5));
+        fragmentArrayList.add(new Fragment("Fragment2", 10, 5));
+        fragmentArrayList.add(new Fragment("Fragment3",10,5));
+        fragmentArrayList.add(new Fragment("Fragment4",10,5));
+
         textView = findViewById(R.id.fragmentCount);
         LayoutInflater inflater = (LayoutInflater)
                 getSystemService(LAYOUT_INFLATER_SERVICE);
         View popupView = inflater.inflate(R.layout.pop_up, null);
+        timerLayoutView = inflater.inflate(R.layout.timer_layout,null);
 
         // create the popup window
         int width = LinearLayout.LayoutParams.WRAP_CONTENT;
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
         boolean focusable = true; // lets taps outside the popup also dismiss it
         final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+        timerLayout = new PopupWindow(timerLayoutView, width, height, false);
 
         showPopUpButton = findViewById(R.id.addFragmentBtn);
         showPopUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               onButtonShowPopupWindowClick(popupWindow,view);
+                onButtonShowPopupWindowClick(popupWindow, view);
             }
         });
         closePopUpButton = popupWindow.getContentView().findViewById(R.id.btnCancel);
         closePopUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dismissPopUp(popupWindow,view);
+                dismissPopUp(popupWindow, view);
             }
         });
 
@@ -67,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         createFragmentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addFragmentToArraylist(popupWindow,view);
+                addFragmentToArraylist(popupWindow, view);
             }
         });
 
@@ -75,58 +94,100 @@ public class MainActivity extends AppCompatActivity {
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                onButtonShowPopupWindowClick(timerLayout,view);
                 onButtonClickPlayTimer(fragmentArrayList);
             }
         });
 
-        fragmentArrayList.add(new Fragment("Fragment1",10,5));
-        fragmentArrayList.add(new Fragment("Fragment2",10,5));
-//        fragmentArrayList.add(new Fragment("Fragment3",60,30));
-//        fragmentArrayList.add(new Fragment("Fragment4",70,30));
-
         fragmentAdapter = new FragmentAdapter(fragmentArrayList);
         LinearLayoutManager linearLayoutManager =
-                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false);
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
 
         fragmentRV = findViewById(R.id.recycleFragment);
         fragmentRV.setLayoutManager(linearLayoutManager);
         fragmentRV.setAdapter(fragmentAdapter);
 
         textView.setText("Total : " + String.valueOf(fragmentAdapter.getItemCount()) + " Fragments");
+
+        countdown = timerLayout.getContentView().findViewById(R.id.txtTime);
+        currentActivity = timerLayout.getContentView().findViewById(R.id.txtRunningActivity);
+        nextActivity = timerLayout.getContentView().findViewById(R.id.txtUpcomingActivity);
+        pauseButton = timerLayout.getContentView().findViewById(R.id.btnPause);
+        resetButton = timerLayout.getContentView().findViewById(R.id.btnReset);
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!timerRunning) {
+                    activeTimerStart();
+                    pauseButton.setText("Pause");
+                    timerRunning=true;
+                }else{
+                    pauseButton.setText("Play");
+                    startTimer.cancel();
+                    timerRunning=false;
+                }
+            }
+        });
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTimer.cancel();
+                itr = fragmentArrayList.iterator();
+                countdown.setText("00");
+                timerRunning=false;
+                StartTimer((Fragment) itr.next());
+            }
+        });
     }
 
     private void onButtonClickPlayTimer(ArrayList<Fragment> fragmentArrayList) {
         itr = fragmentArrayList.iterator();
-        StartTimer((Fragment)itr.next());
+        StartTimer((Fragment) itr.next());
     }
 
     private void StartTimer(Fragment fragment) {
-        final int counter;
-        counter = 1000*fragment.getActiveTime();
-        new CountDownTimer(1000*fragment.getActiveTime(),1000){
+        activeCounter = fragment.getActiveTime();
+        restCounter = fragment.getRestTime();
+        currentActivity.setText(fragment.getTitle());
+        if(itr.hasNext())
+            nextActivity.setText(fragmentArrayList.get(fragmentArrayList.indexOf(fragment) + 1).getTitle());
+        else
+            nextActivity.setText("Last Activity");
+        if(timerRunning)
+            activeTimerStart();
+    }
+
+    private void activeTimerStart(){
+        startTimer = new CountDownTimer(1000 * activeCounter, 1000) {
             @Override
             public void onTick(long l) {
-                playButton.setText(fragment.getTitle() + " Running");
+                activeCounter = (int)(l+1000)/1000;
+                countdown.setText(String.valueOf(activeCounter));
             }
 
             @Override
             public void onFinish() {
-                playButton.setText(("Take Rest"));
-                new CountDownTimer(1000*fragment.getRestTime(),1000){
+                currentActivity.setText("Take Rest");
+                restTimerStart();
+            }
+        }.start();
+    }
 
-                    @Override
-                    public void onTick(long l) {
+    private void restTimerStart(){
+        restTimer = new CountDownTimer(1000 * restCounter, 1000) {
+            @Override
+            public void onTick(long l) {
+                restCounter = (int)(l+1000)/1000;
+                countdown.setText(String.valueOf(restCounter));
+            }
 
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        if(itr.hasNext())
-                            StartTimer((Fragment)itr.next());
-                        else
-                            playButton.setText("Play");
-                    }
-                }.start();
+            @Override
+            public void onFinish() {
+                if (itr.hasNext())
+                    StartTimer((Fragment) itr.next());
+                else
+                    timerLayout.dismiss();
             }
         }.start();
     }
